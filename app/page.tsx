b34ce3d64b86ac3fcb5f81cbb9825901fb6e1a5b
Simon Kamau <simonkamau690@gmail.com>
2023-11-15 13:33:12 +0300

    commiting

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-  <p>Hello World</p>
+  <h1>Betting System</h1>
+  <h2>A Betting System. User Dashboard</h2>
+  <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quia eaque eum ad, fugiat officia, quidem in molestias minus, sed quos! Ipsa molestiae possimus modi, sed pariatur impedit dolore itaque!</h3>
     </main>
   )
 }

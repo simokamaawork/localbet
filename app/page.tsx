1e2c6610f6e8ddc04c2556417628e97167ebd665
Simon Kamau <simonkamau690@gmail.com>
2023-11-27 15:04:58 +0300

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <main className="">
   <h1>Betting System</h1>
-  <h2>A Betting System. User Dashboard</h2>
-  <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quia eaque eum ad, fugiat officia, quidem in molestias minus, sed quos! Ipsa molestiae possimus modi, sed pariatur impedit dolore itaque!</h3>
-    </main>
+   </main>
   )
 }
